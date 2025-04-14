5981a15926e117702dafdc2ee189a1856cbd93c9
John Edwards <uejji@uejji.net>
2025-04-14 00:11:30 +0000

    GPD Pocket 4
    Tested-by: Bart Donnelly <TakunHiwatari@mail2phoenix.com>

--- drivers/gpu/drm/drm_panel_orientation_quirks.c
+++ drivers/gpu/drm/drm_panel_orientation_quirks.c
@@ -341,6 +341,12 @@ static const struct dmi_system_id orientation_data[] = {
 		  DMI_EXACT_MATCH(DMI_BOARD_NAME, "Default string"),
 		},
 		.driver_data = (void *)&gpd_pocket2,
+	}, {	/* GPD Pocket 4 */
+		.matches = {
+		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "GPD"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "G1628-04"),
+		},
+		.driver_data = (void *)&lcd1600x2560_rightside_up,
 	}, {	/* GPD Win (same note on DMI match as GPD Pocket) */
 		.matches = {
 		  DMI_EXACT_MATCH(DMI_BOARD_VENDOR, "AMI Corporation"),
