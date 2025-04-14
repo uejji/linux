e5de41fee56f21beb4ce68483b53f1b818c62a7d
John Edwards <uejji@uejji.net>
2025-04-14 00:09:08 +0000

    AYA NEO 2021 series (alternate vendor id)

--- drivers/gpu/drm/drm_panel_orientation_quirks.c
+++ drivers/gpu/drm/drm_panel_orientation_quirks.c
@@ -214,6 +214,12 @@ static const struct dmi_system_id orientation_data[] = {
 		  DMI_MATCH(DMI_PRODUCT_NAME, "AYA NEO"),
 		},
 		.driver_data = (void *)&lcd800x1280_rightside_up,
+	}, {	/* AYA NEO 2021 series (alternate vendor id) */
+		.matches = {
+		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "AYANEO"),
+		  DMI_MATCH(DMI_PRODUCT_NAME, "NEO 2021"),
+ 		},
+ 		.driver_data = (void *)&lcd800x1280_rightside_up,
 	}, {	/* AYA NEO AIR */
 		.matches = {
 		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "AYANEO"),
